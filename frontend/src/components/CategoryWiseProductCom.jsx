d58fd0dbc1e15662eade80f5d959ed67c27495f6
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-08-18 19:54:28 +0500

    Changes

--- frontend/src/components/CategoryWiseProductCom.jsx
+++ frontend/src/components/CategoryWiseProductCom.jsx
@@ -50,19 +50,19 @@ const CategoryWiseProductCom = () => {
     </div>
   </div>
   <h2 className="text-2xl font-bold mb-6">Products</h2>
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-3/4">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0 w-3/4">
     {products.length ? (
       products.map((product) => (
-        <div key={product.id} className="bg-white shadow-lg rounded-lg p-4">
+        <div key={product.id} className="bg-black shadow-lg rounded-lg p-4">
           <div className="card">
             <img
               src={`http://localhost:8000/${product.image}`}
               className="card-img-top w-full h-64 object-cover"
               alt={product.name}
             />
-            <div className="card-body">
-              <h5 className="card-title text-lg font-bold text-black">{product.name}</h5>
-              <p className="card-text text-black">{product.description}</p>
+            <div className="card-body bg-gray-800" >
+              <h5 className="card-title text-lg font-bold text-white">{product.name}</h5>
+              <p className="card-text text-white">{product.description}</p>
             </div>
           </div>
         </div>
