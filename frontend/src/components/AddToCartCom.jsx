905b7cf0c24ecdefc4550e3720affd96cd4df917
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-08-21 12:07:00 +0500

    Changes

--- frontend/src/components/AddToCartCom.jsx
+++ frontend/src/components/AddToCartCom.jsx
@@ -48,12 +48,6 @@ const AddToCartPage = () => {
               >
                 Remove
               </button>
-              <button
-                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-                onClick={() => handleUpdateCartItem(item)}
-              >
-                Update
-              </button>
             </div>
           </div>
         </div>
