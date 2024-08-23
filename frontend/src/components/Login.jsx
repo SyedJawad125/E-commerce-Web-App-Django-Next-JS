453a655fcce1f61601911e54569a9a6981bf792b
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-08-24 00:03:24 +0500

    Changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import AxiosInstance from "@/components/AxiosInstance";
 import { AuthContext } from "@/components/AuthContext";  // Ensure this import path is correct
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; // FontAwesome icons
 
 const Login = () => {
   const router = useRouter();
@@ -10,6 +12,7 @@ const Login = () => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,43 +35,62 @@ const Login = () => {
   const handleSignup = () => {
     router.push("/signup");
   };
+
+  const handleChangepassword = () => {
+    router.push("/changepassword");
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-700">
-  <div className="w-full max-w-md">
-    <div className="bg-black shadow-md rounded-lg p-8 mt-5">
-      <h3 className="text-center text-2xl font-semibold">Login</h3>
-      <form onSubmit={handleSubmit}>
-        <label className="block text-sm font-medium text-gray-500 mb-2">User Name</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter Username"
-          className="w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-        <label className="block text-sm font-medium text-gray-500  mt-2 mb-2">Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter Password"
-          className="w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-        <button type="submit" className="w-full mt-5 bg-blue-600 text-white font-semibold py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-4">
-          Login
-        </button>
-      </form>
-      <div className="flex justify-end mt-4">
-        <button 
-          onClick={handleSignup} 
-          className="w-1/4 bg-red-600 text-white font-semibold py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
-          Signup
-        </button>
+      <div className="w-full max-w-md">
+        <div className="bg-black shadow-md rounded-lg p-8 mt-5">
+          <h3 className="text-center text-2xl font-semibold">Login</h3>
+          <form onSubmit={handleSubmit}>
+            <label className="block text-sm font-medium text-gray-500 mb-2">User Name</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter Username"
+              className="w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+            <label className="block text-sm font-medium text-gray-500 mt-2 mb-2">Password</label>
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'} // Toggle between text and password
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+                className="w-full text-black px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+              <span
+                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
+              >
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-black" /> {/* Eye icon with black color */}
+              </span>
+            </div>
+            <button type="submit" className="w-full mt-5 bg-blue-600 text-white font-semibold py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-4">
+              Login
+            </button>
+          </form>
+          <div className="flex justify-end mt-4">
+            <button 
+              onClick={handleSignup} 
+              className="w-1/4 bg-red-600 text-white font-semibold py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+              Signup
+            </button>
+          </div>
+          <div className="flex justify-end mt-4">
+            <button 
+              onClick={handleChangepassword} 
+              className="w-1/2 bg-gray-900 text-white font-semibold py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500">
+              Change Password
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
